c2fe6d3f29e7230075722a36a664d1fd24c83c3a
dean-nls <dean@nlsltd.com>
2026-02-17 18:49:19 +0000

    portable text rendering

--- apps/website/src/components/organisms/ExampleSection.tsx
+++ apps/website/src/components/organisms/ExampleSection.tsx
@@ -8,6 +8,21 @@ type exampleSectionProps = Extract<
   { _type: "exampleSection" }
 >;
 
+/**
+ * A simple example section to demonstrate usage of the page builder
+ *
+ * This shouldn't be considered a best practice example of how to write your components.
+ * It's just a basic demonstration with:
+ *   - url generation for an image
+ *   - stega cleaning for page previews in Sanity Studio
+ *   - default styling of portable text
+ *
+ * @param title
+ * @param image
+ * @param orientation
+ * @param copy
+ * @constructor
+ */
 export function ExampleSection({
   title,
   image,
@@ -16,26 +31,29 @@ export function ExampleSection({
 }: exampleSectionProps) {
   return (
     <section
-      className="container mx-auto flex gap-8 py-16 data-[orientation='imageRight']:flex-row-reverse"
+      className="container mx-auto flex gap-8 py-16 flex-col data-[orientation='imageRight']:flex-col-reverse lg:flex-row lg:data-[orientation='imageRight']:flex-row-reverse "
       data-orientation={stegaClean(orientation) || "imageLeft"}
     >
       {image ? (
         <Image
-          className="rounded-xl w-2/3 h-auto"
+          className="rounded-xl max-w-full lg:w-2/3 h-auto"
           src={urlFor(image).width(800).height(600).url()}
           width={800}
           height={600}
           alt=""
         />
       ) : null}
-      <div className="w-1/3 flex items-center">
+      <div className="flex flex-col grow">
         {title ? (
-          <h2 className="text-3xl mx-auto md:text-5xl lg:text-8xl font-light text-pink-500 text-pretty max-w-3xl">
+          <h2 className="text-3xl md:text-5xl lg:text-8xl font-light text-pink-500 max-w-3xl text-center pb-8">
             {title}
           </h2>
         ) : null}
-        {/*copy && (*/}
-        {/*<PortableText blocks={copy} />)*/}
+        {copy && (
+          <div className="prose">
+            <PortableText value={copy} />
+          </div>
+        )}
       </div>
     </section>
   );
