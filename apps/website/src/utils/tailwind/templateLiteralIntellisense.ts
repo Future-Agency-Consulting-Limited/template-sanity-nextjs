d33e1fec1b70b80fb13c551f1e0f2565f5b8a8c1
dean-nls <dean@nlsltd.com>
2026-02-27 16:30:31 +0000

    move tailwind utils to subfolder

--- apps/website/src/utils/tailwind/templateLiteralIntellisense.ts
+++ apps/website/src/utils/tailwind/templateLiteralIntellisense.ts
@@ -34,7 +34,7 @@
  * In a JavaScript or TypeScript file, import the `tw` function and use it in a tagged template literal.
  *
  * @example ```js
- * import { tw } from "utils/tailwindTemplateLiteralIntellisense";
+ * import { tw } from "utils/tailwind/tailwindTemplateLiteralIntellisense";
  * const myString = tw`w-full bg-red-500`
  * ```
  * @see https://github.com/tailwindlabs/tailwindcss-intellisense#extension-settings
