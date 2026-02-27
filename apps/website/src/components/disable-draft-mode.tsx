5a42ebd7d2306209761e408ccd59d35af21957cd
dean-nls <dean@nlsltd.com>
2026-02-27 15:56:12 +0000

    disable draft mode button styling

--- apps/website/src/components/disable-draft-mode.tsx
+++ apps/website/src/components/disable-draft-mode.tsx
@@ -14,7 +14,7 @@ export function DisableDraftMode() {
     // eslint-disable-next-line @next/next/no-html-link-for-pages
     <a
       href="/api/draft-mode/disable"
-      className="fixed bottom-4 right-4 bg-blue-800 rounded-full px-4 py-2 shadow-md border border-gray-500 hover:bg-blue-700 transition-colors"
+      className="fixed bottom-4 right-4  rounded-full px-4 py-2 shadow-md bg-[#6272f9] text-white font-bold border border-gray-600 hover:brightness-110 transition-colors ease-in-out duration-300"
     >
       Disable Draft Mode
     </a>
