39739e94d584efbafa292de3bfeca9582407e4b3
dean-nls <dean@nlsltd.com>
2026-03-26 18:01:04 +0000

    retry now we've proved a basic proxy works

--- apps/website/src/proxy.ts
+++ apps/website/src/proxy.ts
@@ -1,28 +1,20 @@
-// import { NextResponse } from "next/server";
-// //import type { NextRequest } from "next/server";
-//
-// export const config = {
-//   runtime: "nodejs", // edge not supported for proxy in Next.js 16
-//   // match all page routes, excluding api routes and static files (or any file with an extension)
-//   matcher: [
-//     "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
-//   ],
-// };
-//
-// export function proxy(request: Request) {
-//   const country = request.headers.get("x-vercel-ip-country") ?? "UNDETECTED";
-//
-//   console.log("Request to:", request.url);
-//   console.log(`Visitor from ${country}`);
-//
-//   const response = NextResponse.next();
-//   response.headers.set("x-todl-geo-region", country);
-//   return response;
-// }
+import { NextResponse } from "next/server";
+//import type { NextRequest } from "next/server";
 
-export const config = {};
+export const config = {
+  // match all page routes, excluding api routes and static files (or any file with an extension)
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
+  ],
+};
 
 export function proxy(request: Request) {
+  const country = request.headers.get("x-vercel-ip-country") ?? "UNDETECTED";
+
   console.log("Request to:", request.url);
-  return new Response("Logging request URL from Middleware");
+  console.log(`Visitor from ${country}`);
+
+  const response = NextResponse.next();
+  response.headers.set("x-todl-geo-region", country);
+  return response;
 }
