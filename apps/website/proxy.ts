9ca8e337dd407c36564c6792957f089d5a7307cc
dean-nls <dean@nlsltd.com>
2026-03-26 17:19:12 +0000

    run proxy on node instead of unsupported edge runtime

--- apps/website/proxy.ts
+++ apps/website/proxy.ts
@@ -1,16 +1,18 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+//import type { NextRequest } from "next/server";
 
 export const config = {
+  runtime: "nodejs", // edge not supported for proxy in Next.js 16
   // match all page routes, excluding api routes and static files (or any file with an extension)
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
   ],
 };
 
-export function proxy(request: NextRequest) {
+export function proxy(request: Request) {
   const country = request.headers.get("x-vercel-ip-country") ?? "UNKNOWN";
 
+  console.log("Request to:", request.url);
   console.log(`Visitor from ${country}`);
 
   const response = NextResponse.next();
