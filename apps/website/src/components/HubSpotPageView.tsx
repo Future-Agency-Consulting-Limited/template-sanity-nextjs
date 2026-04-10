31a53ab4e67f32d87b7beb16c4e49b4a2273aabb
Dean Lawrence <dl@fg.agency>
2026-04-10 13:19:57 +0100

    tidy up and comments

--- apps/website/src/components/HubSpotPageView.tsx
+++ apps/website/src/components/HubSpotPageView.tsx
@@ -9,21 +9,20 @@ declare global {
   }
 }
 
+/**
+ * Manually trigger tracking for a page view in HubSpot
+ *
+ * Intended to be used as part of a Single Page Application (SPA), as HubSpot's
+ * tracking only automatically fires for a full page load.
+ */
 export default function HubSpotPageView() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
     const _hsq = (window._hsq = window._hsq || []);
-
     _hsq.push(["setPath", window.location.pathname + window.location.search]);
     _hsq.push(["trackPageView"]);
-
-    // _hsq.push([
-    //   "setPath",
-    //   pathname + (searchParams.toString() ? `?${searchParams}` : ""),
-    // ]);
-    // _hsq.push(["trackPageView"]);
   }, [pathname, searchParams]);
 
   return null;
--- apps/website/src/components/HubSpotTracking.tsx
+++ apps/website/src/components/HubSpotTracking.tsx
@@ -3,6 +3,9 @@
 import Script from "next/script";
 import { env } from "@/env/client";
 
+/**
+ * Load HubSpot tracking script
+ */
 export default function HubSpotTracking() {
   if (!env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID) {
     return null;
