e9d1939be97ec97abe70e4d6369803b22920c313
Dean Lawrence <dl@fg.agency>
2026-07-15 13:32:26 +0100

    comments to ease adding components to pagebuilder

--- apps/website/src/components/PageBuilder.tsx
+++ apps/website/src/components/PageBuilder.tsx
@@ -6,6 +6,23 @@ import { createDataAttribute } from "next-sanity";
 import { useOptimistic } from "next-sanity/hooks";
 import HubspotForm from "@/components/patterns/HubspotForm";
 import { ExampleSection } from "@/components/sections/ExampleSection";
+//import { AccordionsSection, AccordionsSectionProps } from "@/components/sections/accordions-section";
+//import { cardCarousel, CardCarouselProps } from "@/components/sections/card-carousel";
+//import { CardGrid, CardGridProps } from "@/components/sections/card-grid";
+//import { ContactSection, ContactSectionProps } from "@/components/sections/contact-section";
+//import { HeroMedia, HeroMediaProps } from "@/components/sections/hero-media";
+//import { HeroSimple, HeroSimpleProps } from "@/components/sections/hero-simple";
+// import {
+//   MarqueeLogos,
+//   MarqueeLogosProps,
+// } from "@/components/sections/marquee-logos";
+//import { MarqueeText, MarqueeTextProps } from "@/components/sections/marquee-text";
+//import { NewsCarousel, NewsCarouselProps } from "@/components/sections/news-carousel";
+//import { NewsGrid, NewsGridProps } from "@/components/sections/news-grid";
+// import {
+//   TwoUpGroup,
+//   TwoUpGroupProps,
+// } from "@/components/sections/two-up-group";
 
 type PageBuilderProps = {
   content: NonNullable<PAGE_QUERY_RESULT>["content"];
@@ -77,6 +94,94 @@ export function PageBuilder({
                 <HubspotForm formId={block.formId} />
               </DragHandle>
             );
+          /*
+          case "accordionsSection":
+            return (
+              <DragHandle key={block._key}>
+                <AccordionsSection {...(block as AccordionsSectionProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "cardCarousel":
+            return (
+              <DragHandle key={block._key}>
+                <CardCarousel {...(block as CardCarouselProps)} />
+              </DragHandle>
+            );
+           */
+          /*
+          case "cardGrid":
+            return (
+              <DragHandle key={block._key}>
+                <CardGrid {...(block as CardGridProps)} />
+              </DragHandle>
+            );
+           */
+          /*
+          case "contactSection":
+            return (
+              <DragHandle key={block._key}>
+                <ContactSection {...(block as ContactSectionProps)} />
+              </DragHandle>
+            );
+           */
+          /*
+          case "heroMedia":
+            return (
+              <DragHandle key={block._key}>
+                <HeroMedia {...(block as HeroMediaProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "heroSimple":
+            return (
+              <DragHandle key={block._key}>
+                <HeroSimple {...(block as HeroSimpleProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "marqueeLogos":
+            return (
+              <DragHandle key={block._key}>
+                <MarqueeLogos {...(block as MarqueeLogosProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "marqueeText":
+            return (
+              <DragHandle key={block._key}>
+                <MarqueeText {...(block as MarqueeTextProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "newsCarousel":
+            return (
+              <DragHandle key={block._key}>
+                <NewsCarousel {...(block as NewsCarouselProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "newsGrid":
+            return (
+              <DragHandle key={block._key}>
+                <NewsGrid {...(block as NewsGridProps)} />
+              </DragHandle>
+            );
+          */
+          /*
+          case "twoUpGroup":
+            return (
+              <DragHandle key={block._key}>
+                <TwoUpGroup {...(block as TwoUpGroupProps)} />
+              </DragHandle>
+            );
+          */
           default: {
             const fallbackBlock = block as {
               _key: string;
