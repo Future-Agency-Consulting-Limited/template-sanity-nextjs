83224b2cdf891230d0d1d828725a235dd74341ac
Dean Lawrence <dl@fg.agency>
2026-07-22 13:58:08 +0100

    pre-add link schema type as it's used in link parser

--- apps/studio/schemaTypes/objects/index.ts
+++ apps/studio/schemaTypes/objects/index.ts
@@ -1,4 +1,8 @@
 import type { SchemaTypeDefinition } from "sanity";
 import { exampleSectionType } from "./exampleSectionType";
+import { linkType } from "./linkType";
 
-export const objectsSchemaTypes: SchemaTypeDefinition[] = [exampleSectionType];
+export const objectsSchemaTypes: SchemaTypeDefinition[] = [
+  exampleSectionType,
+  linkType,
+];
